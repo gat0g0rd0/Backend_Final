2d5198afc5873df728e0a7b88dd86e6fc5824ac7
gat0g0rd0 <109119163+gat0g0rd0@users.noreply.github.com>
2023-03-22 20:19:51 -0300

    v22032019

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM amazoncorretto:11-alpine-jdk
+FROM amazoncorretto:8-alpine-jdk
 MAINTAINER OCP
 COPY target/oscarcalle-0.0.1-SNAPSHOT.jar ocp-backend.jar
 ENTRYPOINT ["java","-jar","/ocp-backend.jar"]
---
+++ src/main/java/com/portfolio/oscarcalle/OscarcalleApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.oscarcalle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OscarcalleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OscarcalleApplication.class, args);
+	}
+
+}
